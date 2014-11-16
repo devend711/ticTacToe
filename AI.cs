72ea2a7438619e221fb6a8e53e8479321e91fea9
Deven <devend711@gmail.com>
2014-11-16 09:17:49 -0500

    init

---
+++ AI.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class AI
+    {
+        public enum CellSelection { N, O, X };
+        private CellSelection[,] grid = new CellSelection[3, 3];
+        private CellSelection playerSelection = CellSelection.X;
+        private CellSelection computerSelection = CellSelection.O;
+
+        public AI(){
+            newGame();
+        }
+        
+        public CellSelection[,] currentGrid()
+        {
+            return this.grid;
+        }
+
+        public Boolean gameOver()
+        {
+            if (didIWin(computerSelection) || didIWin(playerSelection) || numSpacesLeft()==0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void newGame() {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    grid[i, j] = CellSelection.N;
+                }
+            }
+        }
+
+        public Boolean spaceIsEmpty(int i, int j)
+        {
+            return (grid[i, j] == CellSelection.N);
+        }
+
+        public int numSpacesLeft()
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (grid[i,j] != playerSelection && grid[i,j] != computerSelection)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private bool computerMoveAt(int i, int j) // pass the grid by reference so we can change it
+        {
+            if (grid[i, j] == CellSelection.N)
+            {
+                grid[i, j] = computerSelection;
+                return true;
+            }
+            return false;
+        }
+
+        public bool playerMoveAt(int i, int j) // pass the grid by reference so we can change it
+        {
+            if (grid[i, j] == CellSelection.N)
+            {
+                grid[i, j] =  playerSelection;
+                return true;
+            }
+            return false;
+        }
+
+        private int minimax(CellSelection player) // recursivley find a score
+        {
+            if (didIWin(player)) {
+                return 1;
+            } else if (didIWin(opponent(player))) {
+                return -1;
+            }
+
+            int bestRow = -1;
+            int bestCol = -1;
+            int score = -2;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (spaceIsEmpty(i,j))
+                    {
+                        grid[i, j] = player; // try this move
+                        int tempScore = -minimax(opponent(player)); // recursive call
+                        if (tempScore > score) {
+                            score = tempScore;
+                            bestRow = i;
+                            bestCol = j;
+                        }
+                        grid[i,j] = CellSelection.N; // undo the move
+                    }
+                }
+            }
+            if (bestRow == -1 && bestCol == -1) {
+                return 0; // didnt find a good move
+            }
+            return score;
+        }
+
+        public void takeTurn() // recursivley find a best move
+        {
+            if (numSpacesLeft() == 0)
+            {
+                return;
+            }
+            int row = -1;
+            int col = -1;
+            int score = -2;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (spaceIsEmpty(i,j))
+                    {
+                        grid[i, j] = computerSelection;
+                        int tempScore = -minimax(playerSelection);
+                        grid[i, j] = CellSelection.N;
+                        if (tempScore > score)
+                        {
+                            score = tempScore;
+                            row = i;
+                            col = j;
+                        }
+                    }
+                }
+            }
+            computerMoveAt(row, col);
+        }
+
+        private CellSelection opponent(CellSelection player)
+        {
+            return (player == playerSelection ? computerSelection : playerSelection);
+        }
+
+        public Boolean didIWin(CellSelection marker) // returns N if nobody won
+        {
+            int rowScore;
+            int colScore;
+
+            // check rows and columns
+            for (int i = 0; i < 3; i++)
+            {
+                rowScore = 0;
+                colScore = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (grid[i, j] == marker)
+                    {
+                        rowScore += 1;
+                    }
+                    if (grid[j, i] == marker)
+                    {
+                        colScore += 1;
+                    }
+                }
+                if (rowScore == 3 || colScore == 3)
+                {
+                    return true;
+                }
+            }
+
+            // check diagonals
+            if (grid[1, 1] == marker)
+            {
+                if (grid[0,0] == marker && grid[2,2] == marker)
+                {
+                    return true;
+                }
+                if (grid[0, 2] == marker && grid[2, 0] == marker)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D; // For CoordinateSpace
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        private const float clientSize = 100;
+        private const float lineLength = 80;
+        private const float block = lineLength / 3;
+        private const float offset = 10;
+        private const float delta = 5;
+        private float scale;            // Current scale factor
+
+        private AI ai = new AI();
+        private AI.CellSelection player = AI.CellSelection.X;
+        private AI.CellSelection computer = AI.CellSelection.O;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResizeRedraw = true;
+        }
+
+        private void newGame()
+        {
+            ai.newGame();
+            this.Invalidate();
+        }
+
+        private void alertWinner() {
+            if (ai.didIWin(computer))
+            {
+                MessageBox.Show("You lost!");
+                this.computerStartsToolStripMenuItem.Enabled = true;
+            }
+            else if (ai.didIWin(player))
+            {
+                MessageBox.Show("You won!");
+                this.computerStartsToolStripMenuItem.Enabled = true;
+            }
+            else if (ai.numSpacesLeft()==0)
+            {
+                MessageBox.Show("It's a draw!");
+                this.computerStartsToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            ApplyTransform(g);
+
+            if (ai.numSpacesLeft() < 9)
+            {
+                this.computerStartsToolStripMenuItem.Enabled = false;
+            }
+
+            // Draw board
+            g.DrawLine(Pens.Black, block, 0, block, lineLength);
+            g.DrawLine(Pens.Black, 2 * block, 0, 2 * block, lineLength);
+            g.DrawLine(Pens.Black, 0, block, lineLength, block);
+            g.DrawLine(Pens.Black, 0, 2 * block, lineLength, 2 * block);
+            AI.CellSelection[,] grid = ai.currentGrid();
+
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    if (grid[i, j] == AI.CellSelection.O)
+                    {
+                        DrawO(i, j, g);
+
+                    }
+                    else if (grid[i, j] == AI.CellSelection.X)
+                    {
+                        DrawX(i, j, g);
+                    }
+                }
+            }
+        }
+
+        private void ApplyTransform(Graphics g)
+        {
+            scale = Math.Min(ClientRectangle.Width / clientSize, ClientRectangle.Height / clientSize);
+
+            if (scale == 0f) return;
+            g.ScaleTransform(scale, scale);
+            g.TranslateTransform(offset, offset);
+        }
+
+        private void DrawX(int i, int j, Graphics g)
+        {
+            g.DrawLine(Pens.Black, (i * block) + delta, (j * block) + delta, (i * block) + block - delta, (j * block) + block - delta);
+            g.DrawLine(Pens.Black, (i * block) + block - delta, (j * block) + delta, (i * block) + delta, (j * block) + block - delta);
+        }
+
+        private void DrawO(int i, int j, Graphics g)
+        {
+            g.DrawEllipse(Pens.Black, (i * block) + delta, (j * block) + delta, block - (2 * delta), block - (2 * delta));
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            ApplyTransform(g);
+            PointF[] p = { new Point(e.X, e.Y) };
+            g.TransformPoints(CoordinateSpace.World, CoordinateSpace.Device, p);
+
+            if (ai.numSpacesLeft()>0 && !ai.gameOver())
+            {
+                if (p[0].X < 0 || p[0].Y < 0) return;
+                int i = (int)(p[0].X / block);
+                int j = (int)(p[0].Y / block);
+                if (i > 2 || j > 2) return;
+
+                // Only allow setting empty cells
+                if (ai.spaceIsEmpty(i, j))
+                {
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        ai.playerMoveAt(i, j); // pass cell coordinates to AI
+                        this.Invalidate();
+                        alertWinner();
+                        if (!ai.gameOver())
+                        {
+                            ai.takeTurn();
+                            this.Invalidate();
+                            alertWinner();
+                        }
+                    }
+                }
+                else // space is taken
+                {
+                    MessageBox.Show("Bad move!");
+                }
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.newGame();
+        }
+
+        private void computerStartsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.newGame();
+            ai.takeTurn();
+        }
+    }
+}
